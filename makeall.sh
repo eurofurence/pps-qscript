93f37b069861da6526a4cd7547dd6d196d43a634
Dirk Meyer <dirk.meyer+git@dinoex.sub.org>
2023-06-11 14:57:43 +0200

    detect namespace from index.wiki

--- makeall.sh
+++ makeall.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+show="$(grep :events:pps:script: index.wiki | head -1 | cut -d : -f2)"
 cat header.wiki
 for scene in *scene*.wiki
 do
-	echo "{{page>ef26:events:pps:script:${scene%.wiki}}}"
+	echo "{{page>${show}:events:pps:script:${scene%.wiki}}}"
 done
 exit 0
 # eof
